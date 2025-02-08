79c98cd92c6575649381ea88c3c4671845b7d3fd
Assad-lz <assadyssaky@gmail.com>
2025-02-08 10:48:37 -0300

    feat: images is add for the project

--- src/script.js
+++ src/script.js
@@ -7,3 +7,12 @@ function smoothScroll(target) {
     behavior: "smooth",
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav");
+
+  menuToggle.addEventListener("click", function () {
+    nav.classList.toggle("active");
+  });
+});
