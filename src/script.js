28cc58027681456ce00c7626e99a59e4ff0500cf
Assad-lz <assadyssaky@gmail.com>
2025-02-10 15:48:59 -0300

    feat: new sections has inclued in the project

--- src/script.js
+++ src/script.js
@@ -16,3 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
     nav.classList.toggle("active");
   });
 });
+
+// Script para o menu de navegação responsivo
+const mobileMenu = document.getElementById("mobile-menu");
+const navUl = document.querySelector("nav ul");
+
+mobileMenu.addEventListener("click", () => {
+  navUl.classList.toggle("active"); // Adiciona/remove a classe "active"
+});
